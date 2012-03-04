dc5f28433497637dbac4838013de2bac47b79868
Simon Wielens <simonwielens@gmail.com>
2012-03-04 22:50:04 +0000

    choose actor activity

---
+++ src/ie/simo/movies/activities/GetActor.java
@@ -0,0 +1,184 @@
+package ie.simo.movies.activities;
+
+import ie.simo.movies.R;
+import ie.simo.movies.dao.ActorDbAdapter;
+import ie.simo.movies.dao.DirectorDbAdapter;
+import ie.simo.movies.domain.Actor;
+import ie.simo.movies.domain.Cast;
+import ie.simo.movies.domain.Director;
+import ie.simo.movies.domain.MovieInfo;
+import ie.simo.movies.util.DBConsts;
+
+import static ie.simo.movies.util.Consts.BUDGET;
+import static ie.simo.movies.util.Consts.CHOSEN;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GetActor extends Activity {
+
+	private MovieInfo chosenFilm;
+	private TextView chosen;
+	private TextView price;
+	private TextView budgetView;
+	private Spinner spinner;
+	private Button produceFilm;
+	private ActorDbAdapter db;
+	private int budget;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.actors);
+		
+		db = new ActorDbAdapter(this);
+		db.open();
+
+		findAllViewsById();
+		Intent i = getIntent();
+		fillSpinner();
+		
+		chosenFilm = (MovieInfo) i.getSerializableExtra(CHOSEN);
+		budget = i.getIntExtra(BUDGET, 0);
+		chosen.setText(chosenFilm.toButtonText());
+		String msg = "$25,000,000";// TODO get this programmatically - getString(R.string.directorPrice , spinner.getSelectedItem());
+		price.setText(msg);
+		
+		budgetView.setText(budget+"");
+		
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {				
+				
+				if(spinner.getSelectedItem().toString() != null && !spinner.getSelectedItem().toString().equals("") )
+				{					
+					Cursor c = (Cursor) spinner.getSelectedItem();
+					Actor chosenActor = new Actor();
+					chosenActor.setName(c.getString(c.getColumnIndex(DBConsts.Actor.name)));
+					chosenActor.setPriceToHire(Integer.parseInt(c.getString(c.getColumnIndex(DBConsts.Actor.hire_cost))));
+					
+					Cast cast = new Cast();
+					cast.getActors().add(chosenActor);
+					chosenFilm.setCast(cast);
+					//TODO handle this better
+					//chosenFilm.getCast().getActors().add(chosenActor);
+					//format the amount
+					NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+					String msg = getString(R.string.actorPrice , nf.format(chosenActor.getPriceToHire()*1000000));
+					GetActor.this.price.setText(msg);
+					
+					budgetView.setText("$" + (budget - chosenActor.getPriceToHire()));
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				GetActor.this.spinner.setSelection(0);				
+			}
+			
+		});
+		
+		produceFilm.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				if(isValid()){
+					Intent i = new Intent();
+					i.setClass(GetActor.this, Result.class);
+					i.putExtra(CHOSEN, chosenFilm);
+					i.putExtra(BUDGET, budget - chosenFilm.getDirector().getPriceToHire());
+					
+					startActivity(i);
+				}
+				else{
+					makeToast();
+				}
+				
+			}
+		});
+		
+		
+	}
+	
+	private void  makeToast(){
+		Toast.makeText(this, "You can't afford this Actor! Choose again", Toast.LENGTH_SHORT).show();
+
+	}
+	
+	private boolean isValid(){	
+		return (this.budget - chosenFilm.getCast().getCostOfActors() >= 0)? true : false;
+	}
+	
+	
+	
+	
+	private void findAllViewsById() {
+		chosen = (TextView) this.findViewById(R.id.chosen);
+		price = (TextView) this.findViewById(R.id.actorPrice);
+		spinner = (Spinner) this.findViewById(R.id.spinner1);
+		produceFilm = (Button) this.findViewById(R.id.produceFilm);
+		budgetView = (TextView) this.findViewById(R.id.budgetValue);
+	}
+	
+	private void fillSpinner(){
+		 
+		Cursor c = db.fetchAllActors();
+		startManagingCursor(c);
+				
+		// create an array to specify which fields we want to display
+		String[] from = new String[]{DBConsts.Actor.name};
+		// create an array of the display item we want to bind our data to
+		int[] to = new int[]{android.R.id.text1};
+		// create simple cursor adapter
+		SimpleCursorAdapter adapter =
+		  new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item, c, from, to );
+		adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+		// get reference to our spinner
+		spinner.setAdapter(adapter);
+	}
+	
+	 // Initiating Menu XML file (menu.xml)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+ 
+    /**
+     * Event Handling for Individual menu item selected
+     * Identify single menu item by it's id
+     * */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+ 
+        //Doesn't check to see if different button selected
+    	Intent intent = new Intent();
+
+		intent.setClass(this, BoxOffice.class);
+    	
+        return true;
+    }    
+}
---
+++ src/ie/simo/movies/dao/ActorDbAdapter.java
@@ -0,0 +1,36 @@
+package ie.simo.movies.dao;
+
+import ie.simo.movies.util.DBConsts;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class ActorDbAdapter {
+	
+	private Context context;
+	private SQLiteDatabase database;
+	private DatabaseHelper dbHelper;
+
+	public ActorDbAdapter(Context context) {
+		this.context = context;
+	}
+
+	public ActorDbAdapter open() throws SQLException {
+		dbHelper = new DatabaseHelper(context);
+		database = dbHelper.getReadableDatabase();
+		return this;
+	}
+
+	public void close() {
+		dbHelper.close();
+	}
+
+	public Cursor fetchAllActors() {	
+		String query = String.format("select %s, %s, %s, %s from actor order by %s desc", 
+				DBConsts.Actor.id, DBConsts.Actor.id, DBConsts.Actor.name, 
+				DBConsts.Actor.hire_cost, DBConsts.Actor.reputation, DBConsts.Actor.hire_cost);
+		return database.rawQuery(query, null);
+	}
+	
+}
