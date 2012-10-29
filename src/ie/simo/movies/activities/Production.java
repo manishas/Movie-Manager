e53a9e88785e56a885a225dddebc3923193c0830
Simon Wielens <simonwielens@gmail.com>
2012-10-29 23:11:07 +0000

    added fields to production activity

--- src/ie/simo/movies/activities/Production.java
+++ src/ie/simo/movies/activities/Production.java
@@ -2,13 +2,18 @@ package ie.simo.movies.activities;
 
 import ie.simo.movies.R;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Production extends ActivityWithMenu {
+	
+	private ProgressBar productionProgress;
+	private TextView progressText;
+	private ListView productionNews;
 
 	public void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.production);
 	}
 }
