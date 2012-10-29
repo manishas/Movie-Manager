02c5eff7eaec4dc5f6df89ef8b0bb1add06534d0
Simon Wielens <simonwielens@gmail.com>
2012-10-29 23:01:51 +0000

    First commit for production stage

---
+++ src/ie/simo/movies/activities/Production.java
@@ -0,0 +1,14 @@
+package ie.simo.movies.activities;
+
+import ie.simo.movies.R;
+import android.os.Bundle;
+
+public class Production extends ActivityWithMenu {
+
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.production);
+	}
+}
